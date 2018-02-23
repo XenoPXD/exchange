ffa3e4c011832d34f2babf80b04320a41e65e60f
XenoPXD <pxd2001@yahoo.fr>
2018-02-23 10:53:54 +0100

    Add files via upload

--- paper-folding-curve.py
+++ paper-folding-curve.py
@@ -7,7 +7,7 @@ class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescri
     pass
 
 # Print iterations progress
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = 'X'):
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '#'):
     """
     Call in a loop to create terminal progress bar
     @params:
