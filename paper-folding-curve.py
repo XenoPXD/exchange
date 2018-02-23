59f57deacb84a0b525b392cd56a6f40116f75d1a
XenoPXD <pxd2001@yahoo.fr>
2018-02-23 10:53:31 +0100

    Add files via upload

--- paper-folding-curve.py
+++ paper-folding-curve.py
@@ -22,7 +22,7 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    sys.stdout.write('\n\r%s |%s| %s%% %s \n\r' % (prefix, bar, percent, suffix))
+    sys.stdout.write('%s |%s| %s%% %s \r' % (prefix, bar, percent, suffix))
     sys.stdout.flush()
     #print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
     # Print New Line on Complete
